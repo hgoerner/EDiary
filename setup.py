57c56a1570585387379052374ec231bfc845f8f2
Hendrik Görner <hendrikgoerner@gmx.de>
2024-09-30 04:56:08 +0200

    Set up environment variables for API keys and remove unnecessary files

--- setup.py
+++ setup.py
@@ -3,6 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name="pydiary",
     version="0.1",
+    author="Hendrik Görner",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    install_requires=[
+        "requests",
+        "pytest",
+        "folium",
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "scipy",
+        "scikit-learn",
+    ],
 )
