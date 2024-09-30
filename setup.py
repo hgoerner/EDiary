5c68d35e27ecd26b476fa4f66f604adbc30948e8
Hendrik Görner <hendrikgoerner@gmx.de>
2024-09-30 08:56:48 +0200

    Bump version: 1.0.2 → 1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pydiary",
-    version="1.0.2",
+    version="1.0.3",
     author="Hendrik Görner",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
